538127f10f9d4c5ff8c412cf441d426fbd8c8c07
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-09-11 20:04:59 +0000

    trying PUT in useEffect

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -2,54 +2,55 @@ import React, {useState, useEffect} from "react";
 
 const TodoList = () => {
     const [items, setItems] = useState([]);
-
-    useEffect(()=>{
-
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/eduardo', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        console.log(resp.text()); // will try return the exact result as string
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is where your code should start after the fetch finishes
-        setItems(data);
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        //error handling
-        console.log(error);
-    });
-});
-
-/*useEffect(()=>{
-
-  fetch('https://playground.4geeks.com/apis/fake/todos/user/erwin')
-  .then(response => response.json())
-  .then(data => {	
-    setTasks(data)
-  })
-  .catch(error => {
-
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/erwin', {
-      method: "POST",
-      body: JSON.stringify([]),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(data => setTasks([]))
     
-  });
 
-}, [])*/
+    // useEffect to GET user data
+    useEffect(()=>{
+      fetch('https://playground.4geeks.com/apis/fake/todos/user/eduardo')
+      .then(response => {
+          console.log(response.ok); // will be true if the response is successfull
+          console.log(response.status); // the status code = 200 or code = 400 etc.
+          if(response.ok){
+            return response.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+          }
+          throw response;    
+      })
+
+      .then(data => {
+          setItems(data);
+          console.log(data); //this will print on the console the exact object received from the server
+      })
+      .catch(error => {
+          console.error("Error fetching date: ", error);
+          if (error.message.includes("404")) {
+            fetch('https://playground.4geeks.com/apis/fake/todos/user/eduardo', { // this creates the user eduardo if not detected in the server
+              method: 'POST',
+              body: JSON.stringify([]),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+              .then(response => response.json())
+              .then(data => {
+                console.log(data)
+                setItems([])
+              })
+          }    
+      });
+    }, []) 
+
+    // useEffect to PUT user data every time list {items} change
+    useEffect(()=>{
+      const updateList = {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify()
+        };
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/eduardo', updateList)
+        .then((response) => response.json())
+        .then((data) => setItems[items])       
+        .catch((err) => console.log(err.message));
+    }, [items])
 
     return (
         <div className="wrapper">
@@ -62,8 +63,13 @@ const TodoList = () => {
                 <ListDisplay className="list-item" items={items} handleClick={(item) => {
                   setItems(items.filter((i) => i !== item));
                 }}/>
-              <div className="counter">
-                {items.length == null ? 0: items.length} items left
+              <div className="d-flex justify-content-between"> 
+                <div className="counter align-baseline">
+                  {items.length == null ? 0: items.length} items left
+                </div>
+                <DeleteAll className="delete-all" items={items} handleClick={(item) => {
+                  setItems(items=[]);
+                }}/> 
               </div>
           </div>
           <div className="subcontainerone"></div>
@@ -71,19 +77,19 @@ const TodoList = () => {
           </div>
         </div>
       )
-    }
+}
     
     const ListItem = (props) => (
-      <li className="todoitem d-flex justify-content-between">{props.name}
+      <li className="todo-item d-flex justify-content-between">{props.name}
       <button className="deletebtn" onClick={() => props.handleClick(props.name)}>X</button>
       </li>
     )
     
     const ListDisplay = (props) => {
-      const items = props.items.label.map((item, i) => (
+      const items = props.items.map((item, i) => (
         <ListItem
           key={i}
-          name={item}
+          name={item.task}
           handleClick={props.handleClick}
         />
       ))
@@ -106,5 +112,9 @@ const TodoList = () => {
         </form>
       )
     }
+
+    const DeleteAll = (props) => (
+      <button type="button" className="btn btn-primary" onClick={() => props.handleClick(props.name)}>Delete All</button>
+    )
     
     export default TodoList;
